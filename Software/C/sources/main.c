85e1d833ae1f2a05e690916a68b0765abcba896c
Mace Robotics <contact@macerobotics.com>
2020-06-18 18:45:24 +0200

    Update main.c

--- Software/C/sources/main.c
+++ Software/C/sources/main.c
@@ -1,36 +1,21 @@
-#include "MRPi1.h"
-
+#include "MRPiZ.h"
 
 
 int main(int argc, char* argv[])
 {
-unsigned int prox1,prox2,prox3,prox4,prox5;
-
-unsigned int speedFast=50;
-unsigned int speedSlow=35;
 
   // init robot
   init();  
+  
+  printf("Exemple \n");
+  
+  forward(25);
+  
+  sleep(1.5);
+  
+  firmwareVersion();
 
-  while(1)
-  {
-    // lecture des capteurs de proximite
-    prox1 = proxSensor(1);
-    prox2 = proxSensor(2);
-    prox3 = proxSensor(3);
-    prox4 = proxSensor(4);
-    prox5 = proxSensor(5);
-
-    if( prox1>limit_obs and prox2>limit_obs and prox3>limit_obs and prox4>limit_obs and prox5>limit_obs)
-    {
-        back(speedSlow);
-    }
-	else{
-		if(prox1>limit_obs and  prox2>limit_obs)
-		{
-			
-		}
-  }
+  stop();
 
   
 }
