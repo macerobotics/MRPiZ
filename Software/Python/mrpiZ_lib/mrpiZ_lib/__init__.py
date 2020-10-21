1299934d4103005617f9450489c7442b17e4269b
Mace Robotics <contact@macerobotics.com>
2020-10-21 07:02:12 +0200

    Add files via upload

--- Software/Python/mrpiZ_lib/mrpiZ_lib/__init__.py
+++ Software/Python/mrpiZ_lib/mrpiZ_lib/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#  Date : 25/04/2018
-# Version : 0.3.1
+#  Date : 12/10/2020
+# Version : 1.2.2
  
 """
 """
  
-__version__ = "0.3.1"
+__version__ = "1.2.2"
  
 
 from mrpiZ import firmwareVersion
@@ -18,18 +18,19 @@ from mrpiZ import proxSensor
 from mrpiZ import forward
 from mrpiZ import controlEnable
 from mrpiZ import controlDisable
-from mrpiZ import forwardC
+#from mrpiZ import forwardC
 from mrpiZ import back
-from mrpiZ import backC
+#from mrpiZ import backC
 from mrpiZ import stop
 from mrpiZ import turnLeft
-from mrpiZ import turnLeftC
+#from mrpiZ import turnLeftC
 from mrpiZ import turnRight
-from mrpiZ import turnRightC
+#from mrpiZ import turnRightC
 from mrpiZ import forward_mm
 from mrpiZ import back_mm
 from mrpiZ import turnRight_degree
 from mrpiZ import turnLeft_degree
+from mrpiZ import turn_degree
 
 from mrpiZ import robotPositionX
 from mrpiZ import robotPositionY
@@ -38,6 +39,10 @@ from mrpiZ import robotPositionO
 from mrpiZ import motorRight
 from mrpiZ import motorLeft
 from mrpiZ import motorsWrite
+from mrpiZ import motorLeftSpeed
+from mrpiZ import motorRightSpeed
+from mrpiZ import motorsDisable
+from mrpiZ import motorsEnable
 
 from mrpiZ import encoderLeft
 from mrpiZ import encoderRight
@@ -55,3 +60,11 @@ from mrpiZ import forwardmm
 from mrpiZ import backmm
 from mrpiZ import turnRightDegree
 from mrpiZ import turnLeftDegree
+
+from mrpiZ import robotGo
+
+from mrpiZ import buzzer
+from mrpiZ import buzzerStop
+from mrpiZ import buzzerStart
+
+from mrpiZ import ledRGB
